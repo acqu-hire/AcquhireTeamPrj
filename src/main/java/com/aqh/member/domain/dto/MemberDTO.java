cccbecbfe5d30253178ff7c86736f319ba32870a
SiHyeong Kim <makemegrowup4@gmail.com>
2023-01-01 03:17:20 +0900

    Setting

---
+++ src/main/java/com/aqh/member/domain/dto/MemberDTO.java
@@ -0,0 +1,24 @@
+package com.aqh.member.domain.dto;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Component
+@Setter
+@Getter
+@ToString
+public class MemberDTO {
+	private String id; 			// 아이디
+	private String password; 	// 비밀번호
+	private String name; 		// 이름
+	private String gender;		// 성별
+	private String address;		// 주소
+	private String phoneNumber;	// 전화번호
+	private String email;		// 이메일
+	private String regDate;		// 가입일
+}
---
+++ src/test/java/com/aqh/board/DBConnectionTest.java
@@ -0,0 +1,32 @@
+package com.aqh.board;
+
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+@Slf4j
+public class DBConnectionTest {
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Test
+	public void jdbcConnectionTest() throws Exception {
+			Connection connection = dataSource.getConnection();
+			log.info("DB 연결 성공");
+			log.info("Connection >> "+ connection);
+			assertTrue(connection != null);
+	}
+}
