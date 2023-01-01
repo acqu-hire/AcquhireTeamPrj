1d6dd82756cdc0c3b99559693716c9694a463fdf
Devesg <a01094832297@gmail.com>
2023-01-02 02:13:56 +0900

    :wrench: gitignore + (.vscode) + Community DAO, Service , Mapper 추가

---
+++ src/main/java/com/aqh/board/service/CommunityService.java
@@ -0,0 +1,59 @@
+package com.aqh.board.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aqh.board.domain.dao.CommunityBoardDAO;
+import com.aqh.board.domain.dto.BoardDTO;
+
+/**
+ * 커뮤니티 구현 추상화
+ * 
+ * @author Desvesg
+ * 
+ */
+@Service
+public class CommunityService {
+
+    @Autowired
+    private CommunityBoardDAO communityBoardDAO;
+
+    // CREATE
+    public void createPost(BoardDTO boardDTO) {
+        communityBoardDAO.createPost(boardDTO);
+    };
+
+    // READ
+    public List<BoardDTO> getAllCommunityPostList() {
+        return communityBoardDAO.getAllCommunityPostList();
+    };
+
+    public List<BoardDTO> getLifePostList() {
+        return communityBoardDAO.getLifePostList();
+    };
+
+    public List<BoardDTO> getGroupPostList() {
+        return communityBoardDAO.getGroupPostList();
+    };
+
+    public BoardDTO getPost() {
+        return communityBoardDAO.getPost();
+    };
+
+    public long getTotal() {
+        return communityBoardDAO.getTotal();
+    };
+
+    // UPDATE
+    public void updatePost(BoardDTO boardDTO) {
+        communityBoardDAO.updatePost(boardDTO);
+    };
+
+    // DELETE
+    public void deletePost(long bNo) {
+        communityBoardDAO.deletePost(bNo);
+    };
+
+}
