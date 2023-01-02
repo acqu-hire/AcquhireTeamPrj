35115133d0d3fd32dc6a5659d9b8642c780a7538
devesg <a01094832297@gmail.com>
2023-01-02 11:25:54 +0900

    ✅ Add, update, or pass tests + junit builder pattern Test code example + BoardDTO builder pattern apply + BoardDTO variable Long > long Fix

--- src/main/java/com/aqh/member/domain/dto/MemberDTO.java
+++ src/main/java/com/aqh/member/domain/dto/MemberDTO.java
@@ -11,12 +11,12 @@ import lombok.ToString;
 @Getter
 @ToString
 public class MemberDTO {
-	private String id; 			// 아이디
-	private String password; 	// 비밀번호
-	private String name; 		// 이름
-	private String gender;		// 성별
-	private String address;		// 주소
-	private String phoneNumber;	// 전화번호
-	private String email;		// 이메일
-	private String regDate;		// 가입일
-} 
+	private String id; // 아이디
+	private String password; // 비밀번호
+	private String name; // 이름
+	private String gender; // 성별
+	private String address; // 주소
+	private String phoneNumber; // 전화번호
+	private String email; // 이메일
+	private String regDate; // 가입일
+}
