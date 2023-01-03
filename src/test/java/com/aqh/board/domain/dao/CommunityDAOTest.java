7a6e4a0d651654c5069214f6f4185bb6fb24991d
devesg <a01094832297@gmail.com>
2023-01-03 15:21:50 +0900

    ✅ Add, Test Case + communityMapper Fix + CommunityDAO Junit Test Case

--- src/test/java/com/aqh/board/domain/dao/CommunityDAOTest.java
+++ src/test/java/com/aqh/board/domain/dao/CommunityDAOTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.aqh.board.domain.dto.BoardDTO;
+import com.aqh.board.domain.dto.BoardDTO.Category;
+import com.aqh.board.domain.dto.BoardDTO.Menu;
 
 /**
  * @author Devesg
@@ -23,6 +25,35 @@ public class CommunityDAOTest {
 
     @Autowired
     SqlSessionTemplate sessionTemplate;
+
+    @Test
+    public void 커뮤니티_포스트_수정() {
+        BoardDTO boardDTO = BoardDTO.builder()
+                .id("admin")
+                .bNo(12)
+                .menu(Menu.COMMUNITY)
+                .category(Category.COMMUNITY_LIFE)
+                .file("엄")
+                .title("준")
+                .contents("식")
+                .build();
+        System.out.println(boardDTO);
+        sessionTemplate.update("updatePost", boardDTO);
+    }
+
+    @Test
+    public void 커뮤니티_포스트_생성() {
+        BoardDTO boardDTO = BoardDTO.builder()
+                .id("admin")
+                .menu(Menu.COMMUNITY)
+                .category(Category.COMMUNITY_LIFE)
+                .file("12")
+                .title("123")
+                .contents("1234")
+                .build();
+        System.out.println(boardDTO);
+        sessionTemplate.insert("createPost", boardDTO);
+    }
 
     @Test
     public void 커뮤니티_포스트_조회() {
@@ -40,6 +71,13 @@ public class CommunityDAOTest {
     public void 커뮤니티_포스트_개수() {
         long count = (int) sessionTemplate.selectOne("getTotal");
         System.out.println(count);
+    }
+
+    @Test
+    public void 커뮤니티_포스트_삭제() {
+        int bNo = 12;
+        sessionTemplate.delete("deletePost", bNo);
+
     }
 
 }
