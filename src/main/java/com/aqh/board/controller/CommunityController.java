3b25ca947c3bca22994916a9ec0987aaf7d2b331
devesg <a01094832297@gmail.com>
2023-01-04 11:54:43 +0900

    ✅ CommunityController Add returnPath

--- src/main/java/com/aqh/board/controller/CommunityController.java
+++ src/main/java/com/aqh/board/controller/CommunityController.java
@@ -2,17 +2,13 @@ package com.aqh.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aqh.board.service.CommunityService;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Controller
-@RequestMapping("/board/community")
+@RequestMapping("/community")
 public class CommunityController {
 
 	private static final String JSP_ROOT = "board/community";
@@ -41,12 +37,78 @@ public class CommunityController {
 
 	public CommunityService communityService;
 
+	/**
+	 * CREATE PART
+	 * 
+	 * @author Devesg
+	 * @param param
+	 * @return
+	 */
+	@GetMapping(value = "/insert")
+	public Path createCommunityPost() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_INSERT;
+	}
+
+	@GetMapping(value = "/insert_view")
+	public Path createCommunityInsertPost() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_INSERT_VIEW;
+	}
+
+	/**
+	 * READ PART
+	 * 
+	 * @author Devesg
+	 * @param param
+	 * @return
+	 */
 	@GetMapping(value = "/select_all_view")
-	public String getMethodName(Model model) {
-		log.info("" + Path.BOARD_COMMUNITY_SELECT_ALL_VIEW);
-		model.addAttribute("test", communityService.getTotal());
-		log.info("testtetsetest", model);
-		return "" + Path.BOARD_COMMUNITY_SELECT_ALL_VIEW;
+	public Path readCommunityPostList() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_SELECT_ALL_VIEW;
+	}
+
+	@GetMapping(value = "/selectdetail_view")
+	public Path readCommunitPost() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_SELECT_DETAIL_VIEW;
+	}
+
+	/**
+	 * UPDATE PART
+	 * 
+	 * @author Devesg
+	 * @return
+	 */
+	@GetMapping(value = "/update")
+	public Path updateCommunityPost() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_UPDATE;
+	}
+
+	@GetMapping(value = "/update_view")
+	public Path updateCommunityPostView() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_UPDATE_VIEW;
+	}
+
+	/**
+	 * DELETE PART
+	 * 
+	 * @author Devesg
+	 * @return
+	 */
+	@GetMapping(value = "/delete")
+	public Path deleteCommunityPost() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_DELETE;
+	}
+
+	@GetMapping(value = "/delete_view")
+	public Path deleteCommunityPostView() {
+		// TODO: Business logic
+		return Path.BOARD_COMMUNITY_DELETE_VIEW;
 	}
 
 }
