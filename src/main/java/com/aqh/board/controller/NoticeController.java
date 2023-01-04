4be22a873a0bb15b04c970a49b0d9d22b4777d16
kdy <last_1234@naver.com>
2023-01-04 17:42:25 +0900

    notice controller,view_noticeList,view_header 수정

--- src/main/java/com/aqh/board/controller/NoticeController.java
+++ src/main/java/com/aqh/board/controller/NoticeController.java
@@ -1,8 +1,28 @@
 package com.aqh.board.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aqh.board.service.NoticeService;
 
 @Controller
 public class NoticeController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(NoticeController.class);
+	
+	@Autowired
+	private NoticeService noticeService;
+	
+	@RequestMapping(value="notice/menu_select_all_view" , method = RequestMethod.GET)
+	public String menuSelectAll(Model model) {
+		model.addAttribute("menuSelectAll", noticeService.menuSelectAll());
+		LOGGER.info("menuSelectAll",model);
+		return "board/notice/noticeList";
+	}
+	
 
 }
