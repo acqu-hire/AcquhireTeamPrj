7554b547f0e6d679452fa80d7c96ce5984a74621
devesg <a01094832297@gmail.com>
2023-01-11 17:47:18 +0900

    :sparkles: 댓글 Controller,Serivce,Domain 추가

---
+++ src/main/java/com/aqh/reply/domain/dao/ReplyDAO.java
@@ -0,0 +1,15 @@
+package com.aqh.reply.domain.dao;
+
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * 댓글DAO
+ * @author Devesg
+ * @since 23.01.11
+ */
+@Repository
+public class ReplyDAO {
+    
+    private long bNo;
+}
