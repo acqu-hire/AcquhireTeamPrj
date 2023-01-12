bb68304010a11d1c880d1cb4c5612bb880565fb7
SiHyeong Kim <makemegrowup4@gmail.com>
2023-01-12 12:54:51 +0900

    게시판 CRUD 1차 완성

--- src/main/java/com/aqh/board/controller/QnAController.java
+++ src/main/java/com/aqh/board/controller/QnAController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.aqh.board.domain.dto.BoardDTO;
 import com.aqh.board.domain.dto.BoardDTO.Category;
@@ -27,71 +28,79 @@ public class QnAController {
 
 	@GetMapping("/list")
 	public String qnaList(Model model, SearchCondition sc) {
-		PageHandler ph = new PageHandler(sc, service.getBoardCnt(sc));
-		log.info("ph = " + ph);
-		log.info("sc = " + sc);
-		List<BoardDTO> list = service.selectAll(sc);
-		model.addAttribute("ph", ph);
-		model.addAttribute("boardList", list);
-		return "board/qna/qna_list";
+			PageHandler ph = new PageHandler(sc, service.getBoardCnt(sc));
+			model.addAttribute("ph", ph);
+			log.info("ph = " + ph);
+			
+			List<BoardDTO> list = service.selectAll(sc);
+			model.addAttribute("boardList", list);
+			log.info("sc = " + sc);
+			
+			return "board/qna/qna_list";
+		
 	}
 
 	@GetMapping("/listDetail")
 	public String qnaListDetail(long bNo, Model model, SearchCondition sc, BoardDTO boardDTO) {
-		service.readCntUp(bNo);
-		boardDTO = service.selectDetail(bNo);
-		PageHandler ph = new PageHandler(sc, service.getBoardCnt(sc));
-		log.info("ph = " + ph);
-		log.info("boardDTO = " + boardDTO);
-		model.addAttribute(boardDTO);
-		model.addAttribute("ph", ph);
-		return "board/qna/qna_list_detail";
+		
+			service.readCntUp(bNo);
+			boardDTO = service.selectDetail(bNo);
+			
+			model.addAttribute(boardDTO);
+			log.info("boardDTO = " + boardDTO);
+
+			model.addAttribute("sc", sc);
+			log.info("sc = " + sc);
+			
+			return "board/qna/qna_list_detail";
 	}
 
 	@GetMapping("/write")
 	public String qnaInsertForm(Model model, SearchCondition sc) {
-		PageHandler ph = new PageHandler(sc, service.getBoardCnt(sc));
-		model.addAttribute("ph", ph);
+		model.addAttribute("sc", sc);
+		
 		return "board/qna/qna_write_form";
 	}
 
 	@PostMapping("/write")
 	public String qnaInsert(BoardDTO boardDTO, Category category, Model model) {
 		log.info("boardDTO = " + boardDTO);
+		log.info("category = " + category);
+		
 		service.insert(boardDTO);
 		model.addAttribute(category);
+		
 		return "redirect:/QnA/list";
+		
 	}
 
 	@GetMapping("/update")
 	public String qnaUpdateForm(long bNo, Model model, SearchCondition sc) {
+		log.info("sc = " + sc);
+		model.addAttribute("sc", sc);
+		
 		BoardDTO boardDTO = service.selectDetail(bNo);
-		PageHandler ph = new PageHandler(sc, service.getBoardCnt(sc));
-		log.info("boardDTO = " + boardDTO);
-		log.info("ph = " + ph);
 		model.addAttribute(boardDTO);
-		model.addAttribute("ph", ph);
+		log.info("boardDTO = " + boardDTO);
+		
 		return "board/qna/qna_update_form";
+		
 	}
 
 	@PostMapping("/update")
-	public String qnaUpdate(BoardDTO boardDTO, Model model, SearchCondition sc) {
-		service.update(boardDTO);
-		model.addAttribute("page", sc.getPage());
-		model.addAttribute("category", sc.getCategory());
-		model.addAttribute("keyfield", sc.getKeyfield());
-		model.addAttribute("keyword", sc.getKeyword());
-		return "redirect:/QnA/list";
+	public String qnaUpdate(BoardDTO boardDTO, SearchCondition sc) {
+		
+			service.update(boardDTO);
+
+			return "redirect:/QnA/list"+sc.getQueryString();
 	}
 
 	@PostMapping("/delete")
-	public String qnaDelete(long bNo, Model model, SearchCondition sc) {
-		model.addAttribute("page", sc.getPage());
-		model.addAttribute("category", sc.getCategory());
-		model.addAttribute("keyfield", sc.getKeyfield());
-		model.addAttribute("keyword", sc.getKeyword());
+	public String qnaDelete(Integer bNo, RedirectAttributes rattr, SearchCondition sc) {
+		log.info("sc = " + sc);
 		service.delete(bNo);
-		return "redirect:/QnA/list";
+		
+		return "redirect:/QnA/list"+sc.getQueryString();
 	}
 }
 
