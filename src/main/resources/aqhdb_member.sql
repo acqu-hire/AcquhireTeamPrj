0f3364ccc91033e108f12b9ec02e7ebbe6224180
SiHyeong Kim <makemegrowup4@gmail.com>
2023-01-13 12:40:47 +0900

    db sql파일 수정

--- src/main/resources/aqhdb_member.sql
+++ src/main/resources/aqhdb_member.sql
@@ -34,16 +34,6 @@ CREATE TABLE `member` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `member`
---
-
-LOCK TABLES `member` WRITE;
-/*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES ('','','',NULL,'   ','','','2023-01-11 01:01:17'),('aaaa','1234','차차차','F','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-03 12:43:06'),('aabb','12341234','김시형','M','경기 의정부시 가금로34번길 23','01033331111','aaa@naver.com','2023-01-11 11:48:34'),('abc','1234','차차차','F','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-03 10:11:05'),('abcd','1234','차차차','M','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-03 10:07:16'),('aqaq','1234','차차차','F','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-03 10:12:16'),('asdf','1234','차차차','F','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-03 12:42:52'),('bacd','1234','차차차','M','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-03 10:08:14'),('bbb111','12341234','차차차','M','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-10 22:50:26'),('fly123','12341234','차차차','M','제주특별자치도 서귀포시 가가로 14','01033332222','11111@naver.com','2023-01-10 22:52:28');
-/*!40000 ALTER TABLE `member` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -54,4 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-13 12:32:19
+-- Dump completed on 2023-01-13 12:38:20
