534cd4657b6797dab4bf63a39b363fe59887e531
devesg <a01094832297@gmail.com>
2023-01-19 17:50:41 +0900

    :art: 서버 업,다운로드 구현O DB 입출력 구현중

--- src/main/java/com/aqh/board/controller/CommunityController.java
+++ src/main/java/com/aqh/board/controller/CommunityController.java
@@ -1,17 +1,12 @@
 package com.aqh.board.controller;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.aqh.board.domain.dto.BoardDTO;
 import com.aqh.board.domain.dto.Criteria;
@@ -67,9 +62,13 @@ public class CommunityController {
 	}
 
 	@PostMapping(value = "/insert_view")
-	public String createCommunityInsertPost(BoardDTO boardDTO) {
+	public String createCommunityInsertPost(BoardDTO boardDTO, RedirectAttributes rttr) {
 		log.info("PATH " + Path.BOARD_COMMUNITY_INSERT_VIEW);
 
+		System.out.println(boardDTO);
+		if (boardDTO.getAttachList() != null) {
+			boardDTO.getAttachList().forEach(attach -> log.info(attach.toString()));
+		}
 		// communityService.createPost(boardDTO);
 		return Path.BOARD_COMMUNITY_REDIRECT_SELECT_ALL_VIEW.getPath();
 	}
@@ -117,7 +116,11 @@ public class CommunityController {
 	@PostMapping(value = "/update")
 	public String updateCommunityPostView(BoardDTO boardDTO) {
 		log.info("PATH " + Path.BOARD_COMMUNITY_UPDATE_VIEW);
-		communityService.updatePost(boardDTO);
+		System.out.println(boardDTO);
+		if (boardDTO.getAttachList() != null) {
+			boardDTO.getAttachList().forEach(attach -> log.info(attach.toString()));
+		}
+		//communityService.updatePost(boardDTO);
 		return Path.BOARD_COMMUNITY_REDIRECT_SELECT_ALL_VIEW.getPath();
 	}
 
