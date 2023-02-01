4717c3dae2ea5fec25466ec3a1871dea0d82756f
SiHyeong Kim <makemegrowup4@gmail.com>
2023-02-01 15:49:20 +0900

    인터셉터 세션 로그인확인(1차 완성)

--- src/main/java/com/aqh/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/aqh/common/interceptor/LoginInterceptor.java
@@ -2,26 +2,23 @@ package com.aqh.common.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.aqh.member.domain.MemberDTO;
 
 public class LoginInterceptor implements HandlerInterceptor{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		HttpSession session = request.getSession();
-		if(session.getAttribute("id") == null) {
+		String member = (String)request.getSession().getAttribute("id");
+		if(member == null) {
+			response.sendRedirect("/login/login");
+			return false;
 		}
-		return HandlerInterceptor.super.preHandle(request, response, handler);
+		return true;
 	}
 	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-	}
 }
